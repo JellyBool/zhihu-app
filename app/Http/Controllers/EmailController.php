2b749da325ccf714bb0c5386549c81a1d605fab3
JellyBool <jellybool@outlook.com>
2016-12-07 10:59:35 +0800

    zhihu-app lesson 3

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\User;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    public function verify($token)
+    {
+        $user = User::where('confirmation_token',$token)->first();
+
+        if (is_null($user)) {
+            flash('邮箱验证失败','danger');
+            return redirect('/');
+        }
+
+        $user->is_active = 1;
+        $user->confirmation_token = str_random(40);
+        $user->save();
+        Auth::login($user);
+        flash('邮箱验证成功！','success');
+
+        return redirect('/home');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('email/verify/{token}',['as' => 'email.verify', 'uses' => 'EmailController@verify']);
