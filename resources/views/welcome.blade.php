71adf77d28bf83fc1fbe6fa46f95d4ba66d7368d
JellyBool <jellybool@outlook.com>
2016-12-14 21:48:09 +0800

    lesson 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">发现更大的世界</a>
                 </div>
             </div>
         </div>
